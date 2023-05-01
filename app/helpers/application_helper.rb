acb49f85b97ef8889972f93229174288902db102
Moses Gathuku <mosesgathuku95@gmail.com>
2023-05-01 14:50:30 +0300

    Update example image

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,11 @@ module ApplicationHelper
     markdown = Redcarpet::Markdown.new(renderer, extensions)
     markdown.render(content).html_safe
   end
+
+  def example_image(example)
+    asset = Rails.application.assets.find_asset("#{example.namespace}")
+    return "e1.png" if asset.blank?
+
+    "#{example.namespace}.png"
+  end
 end
