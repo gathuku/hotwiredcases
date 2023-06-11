5d9d951543a9df59d89be14f9b167eef9178456e
Rafael Áquila <rafaelaquilaah@hotmail.com>
2023-06-11 17:40:15 -0300

    Fix test names

--- test/controllers/subscribers_controller_test.rb
+++ test/controllers/subscribers_controller_test.rb
@@ -20,7 +20,14 @@ class SubscribersControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-  test "show: should confirm email" do
+  test "create: failure" do
+    assert_no_difference "Subscriber.count" do
+      post subscribers_path, params: { subscriber: { email: "example.com"}}
+    end
+    assert_response :success
+  end
+
+  test "confirm: should confirm email" do
     sid = @subscriber.email_confirmation_tokens.create.signed_id(expires_in: 2.days)
 
     get confirmation_subscribers_path(sid: sid)
@@ -29,7 +36,7 @@ class SubscribersControllerTest < ActionDispatch::IntegrationTest
     assert_equal true, @subscriber.reload.confirmed?
   end
 
-  test "show: should not confirm email with expired token" do
+  test "confirm: should not confirm email with expired token" do
     sid_exp = @subscriber.email_confirmation_tokens.create.signed_id(expires_in: 0.minutes)
 
     get confirmation_subscribers_path(sid: sid_exp, id: @subscriber.id)
@@ -37,12 +44,5 @@ class SubscribersControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to root_path
     assert_equal "That email verification link is invalid", flash[:alert]
     assert_equal false, @subscriber.reload.confirmed?
-  end
-
-  test "create: failure" do
-    assert_no_difference "Subscriber.count" do
-      post subscribers_path, params: { subscriber: { email: "example.com"}}
-    end
-    assert_response :success
   end
 end
