2ed8cccd9f77cc5bdaefa63c35ef70572fbf6d5a
Rafael Áquila <rafaelaquilaah@hotmail.com>
2023-06-11 17:46:27 -0300

    Add postmark gem

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,6 +63,8 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "hotwiredexamples_production"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.delivery_method = :postmark
+  config.action_mailer.postmark_settings = { api_token: Rails.application.credentials.postmark_api_token }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
