2929f69483141825b41244d75505d26a6f6dda79
Moses Gathuku <mosesgathuku95@gmail.com>
2023-06-13 19:10:24 +0300

    Update controller

--- app/controllers/subscriber_confirmations_controller.rb
+++ app/controllers/subscriber_confirmations_controller.rb
@@ -2,15 +2,17 @@ class SubscriberConfirmationsController < ApplicationController
   before_action :set_subscriber, only: :show
 
   def show
-    @subscriber.confirm!
-    redirect_to root_path, notice: "Thank you for verifying your email address"
+    if @subscriber.present?
+      @subscriber.confirm!
+      redirect_to root_path, notice: "Thank you for verifying your email address"
+    else
+      redirect_to root_path, alert: "That email verification link is invalid"
+    end
   end
 
   private
 
   def set_subscriber
-    token = EmailConfirmationToken.find_signed!(params[:sid]); @subscriber = token.subscriber
-  rescue StandardError
-    redirect_to root_path, alert: "That email verification link is invalid"
+    @subscriber = GlobalID::Locator.locate_signed(params[:subscriber_id])
   end
 end
--- test/controllers/subscriber_confirmations_controller_test.rb
+++ test/controllers/subscriber_confirmations_controller_test.rb
@@ -3,24 +3,20 @@ require "test_helper"
 class SubscriberConfirmationsControllerTest < ActionDispatch::IntegrationTest
   setup do
     @subscriber = subscribers(:one)
+    assert_equal false, @subscriber.reload.confirmed?
   end
 
   test "confirm: should confirm email" do
-    sid = @subscriber.email_confirmation_tokens.create.signed_id(expires_in: 2.days)
-
-    get subscriber_confirmation_path(@subscriber, sid: sid)
+    get subscriber_confirmation_path(@subscriber.to_sgid.to_s)
+    assert_equal true, @subscriber.reload.confirmed?
 
     assert_redirected_to root_url
-    assert_equal true, @subscriber.reload.confirmed?
   end
 
   test "confirm: should not confirm email with expired token" do
-    sid_exp = @subscriber.email_confirmation_tokens.create.signed_id(expires_in: 0.minutes)
-
-    get subscriber_confirmation_path(@subscriber, sid: sid_exp)
-
-    assert_redirected_to root_path
+    get subscriber_confirmation_path(@subscriber.to_sgid(expires_in: 0.seconds))
     assert_equal "That email verification link is invalid", flash[:alert]
     assert_equal false, @subscriber.reload.confirmed?
+    assert_redirected_to root_path
   end
 end
