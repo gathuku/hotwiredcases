5df6f055722fd800f2e0be66b16a419ee0cf5fd2
Moses Gathuku <mosesgathuku95@gmail.com>
2023-06-19 15:39:44 +0300

    Add examples in seeds (#17)

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,14 @@
 
+puts "Seeding..."
+
+Example.create!([
+  { name: "Inline Editing", namespace: "e1", src_path: "/e1/products/:product_id" },
+  { name: "Modal", namespace: "e2", src_path: "/e2/products" },
+  { name: "Typehead searching", namespace: "e3", src_path: "/e3/products" },
+  { name: "Lazy loading", namespace: "e4", src_path: "/e1/products/:product_id" }
+])
+
+
 100.times do
   Product.create(
     {
@@ -8,3 +18,5 @@
     }
   )
 end
+
+puts "Seeding done!"
