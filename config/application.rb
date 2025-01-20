d41374292df23ec52b68899a77fdda809f59ca2f
Moses Gathuku <mosesgathuku95@gmail.com>
2025-01-20 22:11:34 +0300

    Update files

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,11 @@ module Hotwiredexamples
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    # Please, add to the `ignore` list any other `lib` subdirectories that do
+    # not contain `.rb` files, or that should not be reloaded or eager loaded.
+    # Common ones are `templates`, `generators`, or `middleware`, for example.
+    config.autoload_lib(ignore: %w[assets tasks])
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
@@ -18,10 +23,5 @@ module Hotwiredexamples
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-    config.generators do |g|
-      g.helper false
-    end
-
-    config.active_job.queue_adapter = :sidekiq
   end
 end
