c0f94b9888a8781bc3d681163754ce0071092a01
Moses Gathuku <mosesgathuku95@gmail.com>
2025-01-20 19:58:42 +0300

    Update config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -49,7 +49,7 @@ Rails.application.configure do
   # config.action_cable.allowed_request_origins = [ "http://example.com", /http:\/\/example.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force_ssl = true
+  config.force_ssl = true
 
   # Include generic and useful information about system operation, but avoid logging too much
   # information to avoid inadvertent exposure of personally identifiable information (PII).
@@ -95,4 +95,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.ssl_options = { redirect: { exclude: ->(request) { request.path == “/up” } }
 end
