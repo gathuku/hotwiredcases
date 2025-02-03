a5faee097bdf29afe78f2665d82821665c0d7cc1
Moses Gathuku <mosesgathuku95@gmail.com>
2025-02-03 23:48:10 +0300

    update example

--- app/javascript/controllers/frame_controller.js
+++ app/javascript/controllers/frame_controller.js
@@ -1,11 +1,21 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
   static values = {
-    reloadInterval: 1000
-  }
+    reloadInterval: 1000,
+  };
 
   connect() {
-    setInterval(() => { this.element.reload() }, this.reloadIntervalValue)
+    // setInterval(() => { this.element.reload() }, this.reloadIntervalValue)
+    let a = 0;
+
+    this.intervalId = setInterval(() => {
+      if (a >= 10) {
+        clearInterval(this.intervalId);
+      } else {
+        this.element.reload();
+        a++;
+      }
+    }, 1000);
   }
 }
